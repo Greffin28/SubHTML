43572b768821bc9f63f85e308cb828c987577715
Greffin28 <greffinroid@ymail.com>
2018-07-07 20:07:06 +0700

    Now using C++14 experimental filesystem
    Much cleaner and understandable code
    Also enables auto detecting .subhtml files

--- src/SubHTML.cpp
+++ src/SubHTML.cpp
@@ -1,74 +1,50 @@
 #include <iostream>
 #include <string>
-#include <cstring>
 #include <vector>
 #include <fstream>
+#include <filesystem>
 
 std::vector<std::string> files;
-std::string inFolder = "";
+std::vector<std::string> ignoredFiles;
 
-std::string folderPath(std::string &filePath) {
-	int foundAt = filePath.rfind('/');
-	if (foundAt >= filePath.length()) return "";
-	else return filePath.substr(0, foundAt);
+std::string getFolderPath(std::experimental::filesystem::path filePath) {
+	return filePath.string().substr(0, filePath.string().find(filePath.filename().string()));
 }
 
-std::string fileNameE(std::string &filePath) {
-	int folderLength = folderPath(filePath).length();
-	if (folderLength > 0) return filePath.substr(folderLength + 1, filePath.length() - folderLength - 1);
-	else return filePath;
+std::string getFileNameWithoutExtension(std::experimental::filesystem::path filePath) {
+	return filePath.filename().string().substr(0, filePath.filename().string().find(filePath.extension().string()));
 }
 
-std::string fileNameNE(std::string &filePath) {
-	int folderLength = folderPath(filePath).length();
-	if (folderLength > 0) return filePath.substr(folderLength + 1, filePath.rfind('.') - folderLength - 1);
-	else return filePath.substr(0, filePath.rfind('.'));
-}
+void processFile(std::experimental::filesystem::path filePath) {
+	std::cout << "Processing: " << filePath << std::endl;
 
-void fixFolderFileName(std::string &folder, std::string &fileName) {
-	if (folderPath(fileName).length() > 0) {
-		std::string separator = "";
-		if (folder.length() > 0) separator = "/";
-
-		folder += separator + folderPath(fileName);
-		fileName = fileNameE(fileName);
-	}
-}
-
-void processFile(std::string folder, std::string fileName, std::string postfix) {
 	std::ifstream is, is2;
 	std::ofstream os;
 
-	fixFolderFileName(folder, fileName);
+	std::string folderName = getFolderPath(filePath);
+	std::string fileNameNoExtension = getFileNameWithoutExtension(filePath);
 
-	std::string separator = "";
-	if (folder.length() > 0) separator = "/";
-
-	is.open((folder + separator + fileName).c_str());
+	is.open(filePath);
 	if (is.is_open()) {
-		os.open((folder + separator + fileNameNE(fileName) + postfix).c_str());
+
+		os.open(folderName + fileNameNoExtension + ".html");
 		if (os.is_open()) {
 			std::string line;
 			while (getline(is, line)) {
 				if (line.length() > 2 && line.at(0) == '[' && line.at(line.length() - 1) == ']') {
-					line = line.substr(1, line.length() - 2);
+					auto subFilePath = std::experimental::filesystem::path(line.substr(1, line.length() - 2));
+					std::string subFileFolderName = getFolderPath(subFilePath);
+					std::string subFileNameNoExtension = getFileNameWithoutExtension(subFilePath);
 
-					std::string relativeFolder = folder;
-
-					fixFolderFileName(relativeFolder, line);
-
-					std::string separator = "";
-					if (relativeFolder.length() > 0) separator = "/";
-
-					processFile(relativeFolder, line, ".temp");
-					is2.open((relativeFolder + separator + fileNameNE(line) + ".temp").c_str());
+					processFile(folderName + subFilePath.string());
+					is2.open(folderName + subFileFolderName + subFileNameNoExtension + ".html");
 					if (is2.is_open()) {
 						while (getline(is2, line)) {
 								os << line << std::endl;
 						}
 						is2.close();
 					} else {
-							std::cout << "Error on opening file: " << relativeFolder + separator + fileNameNE(line) + ".temp" << std::endl;
+							std::cout << "Error on opening file: " << folderName << subFileFolderName << subFileNameNoExtension << ".html" << std::endl;
 					}
 				} else {
 					os << line << std::endl;
@@ -76,37 +52,70 @@ void processFile(std::string folder, std::string fileName, std::string postfix) 
 			}
 			os.close();
 		} else {
-			std::cout << "File path doesn't exist: " << folder + separator + fileName + postfix << std::endl;
+			std::cout << "File path doesn't exist: " << folderName << fileNameNoExtension << ".html" << std::endl;
 		}
 		is.close();
 	} else {
-		std::cout << "Error on opening file: " << folder + separator + fileName << std::endl;
+		std::cout << "Error on opening file: " << filePath.string() << std::endl;
+	}
+}
+
+void includeFile(std::experimental::filesystem::path filePath) {
+	if (filePath.extension().string() == ".subhtml") {
+		files.push_back(filePath.string());
+	} else {
+		ignoredFiles.push_back(filePath.string());
+	}
+}
+
+void includeFolder(std::experimental::filesystem::path folderPath) {
+	std::experimental::filesystem::directory_iterator dirIter(folderPath);
+
+	for (auto & fileIter : dirIter) {
+		auto filePath = std::experimental::filesystem::path(fileIter);
+
+		if (std::experimental::filesystem::is_directory(filePath)) {
+			includeFolder(filePath);
+		} else if (std::experimental::filesystem::is_regular_file(filePath)) {
+			includeFile(filePath);
+		}
 	}
 }
 
 int main() {
+	
 	std::ifstream fileList("subhtml.txt");
 
 	if (fileList.is_open()) {
 		std::string line;
 		while (getline(fileList, line)) {
 			if (line == "" || line.at(0) == '#') {
-				// Do Nothing
-			} else if (line != "" && line.at(0) == '>') {
-				inFolder = line.substr(1, line.length() - 1);
-			} else {
-				files.push_back(inFolder + line);
+				continue;
+			}
+
+			auto path = std::experimental::filesystem::path(line);
+			
+			if (std::experimental::filesystem::is_regular_file(path)) {
+				includeFile(path);
+			} else if (std::experimental::filesystem::is_directory(path)) {
+				includeFolder(path);
 			}
 		}
 
-		for (int i = 0; i < files.size(); ++i) {
-			processFile(folderPath(files.at(i)), fileNameE(files.at(i)), ".html");
+		std::cout << ignoredFiles.size() << " ignored files:" << std::endl;
+		for (std::string & filePath : ignoredFiles) {
+			std::cout << filePath << std::endl;
+		}
+
+		std::cout << std::endl << "================================\n" << files.size() << " processed files:" << std::endl;
+		for (std::size_t i = 0; i < files.size(); ++i) {
+			processFile(std::experimental::filesystem::path(files.at(i)));
 		}
 		
 		fileList.close();
 	} else {
 		std::cout << "No subhtml.txt file exist!" << std::endl;
 	}	
-
+	
 	return 0;
 }
